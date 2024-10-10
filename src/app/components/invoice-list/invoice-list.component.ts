911c8b7150bc8d11847562bfbf60dc1c5a0899fc
Vinaykd24 <vinay.kadam24@gmail.com>
2024-10-10 10:12:42 +0530

    Added pagination and sorting

--- src/app/components/invoice-list/invoice-list.component.ts
+++ src/app/components/invoice-list/invoice-list.component.ts
@@ -1,26 +1,34 @@
-import {
-  Component,
-  OnInit,
-  Signal,
-  signal,
-  WritableSignal,
-} from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { VehicleFacadeService } from '../../Services/vehicle-facade.service';
 import { CommonModule } from '@angular/common';
-import { Invoice, UpdatedInvoice } from '../../models/common.models';
-import { MatTableModule } from '@angular/material/table';
+import { UpdatedInvoice } from '../../models/common.models';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-invoice-list',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSortModule,
+    MatPaginatorModule,
+  ],
   templateUrl: './invoice-list.component.html',
   styleUrl: './invoice-list.component.scss',
 })
-export class InvoiceListComponent implements OnInit {
-  invoices!: Signal<Invoice[]>;
+export class InvoiceListComponent implements OnInit, AfterViewInit {
   invoiceList$!: Observable<UpdatedInvoice[]>;
+  dataSource = new MatTableDataSource<UpdatedInvoice>();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   displayedColumns: string[] = [
     'model',
     'city',
@@ -32,13 +40,48 @@ export class InvoiceListComponent implements OnInit {
     'dropTime',
     'totalCost',
   ];
-  dataSource: Invoice[] = [];
 
-  constructor(private vehicleFacadeService: VehicleFacadeService) {
-    this.invoiceList$ = this.vehicleFacadeService.getAllInvoicesFromDb();
-  }
+  constructor(
+    private vehicleFacadeService: VehicleFacadeService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.invoices = this.vehicleFacadeService.getInvoicesListSignal();
+    this.loadInvoices();
+  }
+
+  ngAfterViewInit() {
+    // Set up the paginator after the view has been initialized
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  loadInvoices() {
+    this.vehicleFacadeService.getAllInvoicesFromDb().subscribe(
+      (data) => {
+        this.dataSource.data = data;
+        // Ensure the paginator is set after data is loaded
+        setTimeout(() => {
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+        });
+      },
+      (error) => {
+        console.error('Error fetching invoices:', error);
+      }
+    );
+  }
+
+  viewInvoice(id: string) {
+    return this.router.navigate([`/invoice/${id}`]);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
